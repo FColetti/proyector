2ae98ced39f14ce7799c2c6118050e3697d4ac9d
eric.munoz1701 <eric.munoz1701@alumnos.ubiobio.cl>
2022-09-03 20:28:59 -0400

    v 0.3

--- 4.R
+++ 4.R
@@ -48,10 +48,17 @@ health$smoking_status[health$smoking_status =="formerly smoked"] <- 1
 health$smoking_status[health$smoking_status =="never smoked"] <- 0
 health$smoking_status[health$smoking_status =="smokes"] <- 1
 
+#columna sobre bmi status
+health$bmi_status[health$bmi < 18.5 ] <- "Bajo peso"
+health$bmi_status[health$bmi >= 18.5 & health$bmi< 24.9] <- "Saludable"
+health$bmi_status[health$bmi >= 24.9 & health$bmi< 29.9] <- "Sobrepeso"
+health$bmi_status[health$bmi >= 29.9 & health$bmi< 34.9] <- "Obesidad tipo 1"
+health$bmi_status[health$bmi >= 34.9 & health$bmi< 39.9] <- "Obesidad tipo 2"
+health$bmi_status[health$bmi > 39.9] <- "Obesidad tipo 3"
 
 
 health$age_group <- cut(health$age, breaks = seq(30, 110, 10))
-health$bmi_group <- cut(as.numeric(health$bmi), breaks = seq(15, 60, 5.5))
+health$bmi_group <- cut(as.numeric(health$bmi), breaks = seq(15, 60, 2.5))
 
 strokes_by_age <- health %>%
     group_by(age_group) %>%
@@ -102,9 +109,15 @@ disease_by_smoke <- health %>%
   summarise(target = sum(heart_disease ==1))
 
 #enfermedades x peso
+disease_by_bmi <- health %>%
+  group_by(bmi_status) %>%
+  arrange(bmi_status) %>%
+  summarise(target = sum(heart_disease == 1))
+
+typeof(disease_by_bmi)
 
 
-
+bmi_listtt<-disease_by_bmi[nrow(disease_by_bmi):1,]
 
 #GRAFICOS
 
@@ -137,6 +150,10 @@ ggplot(strokes_by_smoke, aes(x = smoking_status, y = target)) +
   geom_bar(stat = "identity", fill = "blue") +
   labs(title = "Strokes by smoke", x = "Age", y = "Strokes")
 
+#grafico de bmi x enfermedades
+ggplot(disease_by_bmi, aes(x = bmi_status, y = target)) +
+  geom_bar(stat = "identity", fill = "blue") +
+  labs(title = "bmi by disease", x = "bmi", y = "disease")
 
 # strokes_by_gender <- health %>%
 #     filter(health$hypertension == 1) %>%
